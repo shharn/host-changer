817c7d82fb35ff8cb7aad8565483f4268ebfc03c
한승환 [shharn2] <shharn2@nexon.co.kr>
2018-07-19 13:41:21 +0900

    be faster

--- src/main/main.go
+++ src/main/main.go
@@ -22,89 +22,96 @@ var (
 )
 
 func main() {
-	switchCommand := flag.NewFlagSet("switch", flag.ExitOnError)
-	targetEnvironment := switchCommand.String("env", "live", "Target Environment you want to work. ( local | dev | test | pre | live )")
+	var wg sync.WaitGroup
+	wg.Add(4)
 
-	if len(os.Args) < 2 {
-		fmt.Println(`- Usage
-			host-changer [command] [flag] [value] [value] ...
-			
-			- Available Commands
-			switch
-			`)
-		os.Exit(1)
-	}
+	go func() {
+		switchCommand := flag.NewFlagSet("switch", flag.ExitOnError)
+		targetEnvironment := switchCommand.String("env", "live", "Target Environment you want to work. ( local | dev | test | pre | live )")
 
-	switch os.Args[1] {
-	case "switch":
-		switchCommand.Parse(os.Args[2:])
-	default:
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
+		if len(os.Args) < 2 {
+			fmt.Println(`- Usage
+				host-changer [command] [flag] [value] [value] ...
+				
+				- Available Commands
+				switch
+				`)
+			os.Exit(1)
+		}
 
-	hosts := switchCommand.Args()
-	if switchCommand.Parsed() {
-		fmt.Printf("targetEnvironment : %s, hosts : %s\n", *targetEnvironment, hosts)
-	}
+		switch os.Args[1] {
+		case "switch":
+			switchCommand.Parse(os.Args[2:])
+		default:
+			flag.PrintDefaults()
+			os.Exit(1)
+		}
 
-	content, err := ioutil.ReadFile(hostFilePath)
-	if err != nil {
-		fmt.Println("Fail to read file")
-		os.Exit(1)
-	}
+		hosts := switchCommand.Args()
+		if switchCommand.Parsed() {
+			fmt.Printf("targetEnvironment : %s, hosts : %s\n", *targetEnvironment, hosts)
+		}
 
-	splitted := strings.Split(string(content), nextLine)
-	currentHostGroup := ""
-	for idx := 0; idx < len(splitted); idx++ {
-		singleLine := strings.TrimSpace(splitted[idx])
-		if len(singleLine) > 0 && isHostGroupDeclaration(singleLine) {
-			temp := strings.TrimSpace(strings.Split(singleLine, "###")[1])
-			if _, exists := contains(hosts, temp); exists == true {
-				currentHostGroup = temp
-			} else {
-				currentHostGroup = ""
-			}
-		} else if len(currentHostGroup) > 0 {
-			tempHost := strings.Fields(singleLine)
-			if len(tempHost) == 2 && tempHost[1] == currentHostGroup {
-				tempHostIP := strings.TrimSpace(tempHost[0])
-				tempEnvStr := *targetEnvironment
-				targetHostList := hostEnvMap[tempEnvStr]
-				if isHostIPCommented(tempHostIP) {
-					tempHostIP = tempHostIP[1:]
-					if *targetEnvironment != "live" && isTargetEnvHostIP(targetHostList, tempHostIP) {
-						fmt.Printf("Will remove the Hashbang : %v\n", tempHostIP)
-						// remove leading hashbang
-						singleLine = singleLine[1:]
-					}
-				} else { // In case of already set host ip
-					if *targetEnvironment == "live" || !isTargetEnvHostIP(targetHostList, tempHostIP) {
-						fmt.Printf("Will comment this line out : %v\n", tempHostIP)
-						singleLine = "#" + singleLine
-					}
+		content, err := ioutil.ReadFile(hostFilePath)
+		if err != nil {
+			fmt.Println("Fail to read file")
+			os.Exit(1)
+		}
+
+		splitted := strings.Split(string(content), nextLine)
+		currentHostGroup := ""
+		for idx := 0; idx < len(splitted); idx++ {
+			singleLine := strings.TrimSpace(splitted[idx])
+			if len(singleLine) > 0 && isHostGroupDeclaration(singleLine) {
+				temp := strings.TrimSpace(strings.Split(singleLine, "###")[1])
+				if _, exists := contains(hosts, temp); exists == true {
+					currentHostGroup = temp
+				} else {
+					currentHostGroup = ""
 				}
-				splitted[idx] = singleLine
+			} else if len(currentHostGroup) > 0 {
+				tempHost := strings.Fields(singleLine)
+				if len(tempHost) == 2 && tempHost[1] == currentHostGroup {
+					tempHostIP := strings.TrimSpace(tempHost[0])
+					tempEnvStr := *targetEnvironment
+					targetHostList := hostEnvMap[tempEnvStr]
+					if isHostIPCommented(tempHostIP) {
+						tempHostIP = tempHostIP[1:]
+						if *targetEnvironment != "live" && isTargetEnvHostIP(targetHostList, tempHostIP) {
+							fmt.Printf("Will remove the Hashbang : %v\n", tempHostIP)
+							// remove leading hashbang
+							singleLine = singleLine[1:]
+						}
+					} else { // In case of already set host ip
+						if *targetEnvironment == "live" || !isTargetEnvHostIP(targetHostList, tempHostIP) {
+							fmt.Printf("Will comment this line out : %v\n", tempHostIP)
+							singleLine = "#" + singleLine
+						}
+					}
+					splitted[idx] = singleLine
+				}
 			}
 		}
-	}
-	result := strings.Join(splitted, nextLine)
-	ioutil.WriteFile(hostFilePath, []byte(result), 0664)
+		result := strings.Join(splitted, nextLine)
+		ioutil.WriteFile(hostFilePath, []byte(result), 0664)
+		wg.Done()
+	}()
 
-	var wg sync.WaitGroup
-	wg.Add(3)
 	go func() {
 		defer wg.Done()
 		clearTempFileOfIE()
 	}()
+
 	go func() {
 		defer wg.Done()
 		clearTempFileOfChrome()
 	}()
+
 	go func() {
 		defer wg.Done()
 		flushDNSCache()
 	}()
+
 	wg.Wait()
 }
 
@@ -161,6 +168,6 @@ func flushDNSCache() {
 func execCommand(errorMessage, name string, args ...string) {
 	cmd := exec.Command(name, args...)
 	if err := cmd.Run(); err != nil {
-		fmt.Printf("%s - %s", errorMessage, err.Error())
+		fmt.Printf("%s - %s\n", errorMessage, err.Error())
 	}
 }
